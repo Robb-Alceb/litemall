43c89ed8f2d5c1f25c95241777e6659a6f96a72a
liushichang <2861322722@qq.com>
2020-02-24 11:34:47 +0800

    1.ipad下班结算面额配置读取

---
+++ litemall-web-api/src/main/java/org/linlinjava/litemall/web/web/WebConfigController.java
@@ -0,0 +1,29 @@
+package org.linlinjava.litemall.web.web;
+
+import org.linlinjava.litemall.core.util.ResponseUtil;
+import org.linlinjava.litemall.db.service.LitemallSystemConfigService;
+import org.linlinjava.litemall.web.annotation.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：stephen
+ * @date ：Created in 2020/2/24 10:52
+ * @description：系统配置读取
+ */
+@RestController
+@RequestMapping("/web/config")
+public class WebConfigController {
+    @Autowired
+    private LitemallSystemConfigService systemConfigService;
+
+    @GetMapping("settlement")
+    public Object listSettlement(@LoginUser Integer userId){
+        if(userId == null){
+            return ResponseUtil.unlogin();
+        }
+        return ResponseUtil.ok(systemConfigService.listSettlement());
+    }
+}
